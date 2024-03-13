5d2f8d12d6a276215414fb6d3dc18aae341be05e
ph1122 <923890618@qq.com>
2024-03-13 20:58:04 +0800

    chapter 1

---
+++ chapter_2/2_1.c
@@ -0,0 +1,13 @@
+// first.c
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	num = 1;
+
+	printf("I an a simple ");
+	printf("computer.\n");
+	printf("My favorite number is %d become it is first.\n", num);
+
+	return 0;
+}
